df1a706efc27dd085015e889ee7d485f8085173e
Timothy Anthony <pilot.taim@gmail.com>
2017-08-16 09:47:26 +0300

    Adding api_key field in users migration file

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        User::truncate();
+
+        // Creating a demo user in our database.
+        Pharmacy::create([
+            'name' => "Administrator",
+            'email' => "admin@codefortanzania.org",
+            'password' => bcrypt('123456'),
+            'api_key' => str_random(60)
+        ]);
+    }
+}
